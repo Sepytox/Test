e7bbc65b56e4898c55da061010f4c20c1731b2bc
Sepy <lacostem@bzz.ch>
2022-10-07 16:13:06 +0200

    @index added: -navbar -login color -added stylesheet
    @js
    added:
    -class to edit button
    
    @login.html
    added:
    -login is hidden after login

--- index.js
+++ index.js
@@ -19,24 +19,23 @@ function renderTasks(tasks) {
 
         const tableRow = document.createElement('tr');
         const deletebutton = document.createElement("button");
+        deletebutton.classList.add("btn", "btn-danger");
         const editbutton = document.createElement("button");
-        const checkbox = document.createElement("input");
-        checkbox.type = "checkbox"
+        editbutton.classList.add("btn", "btn-outline-primary");
 
-        editbutton.innerText = "Edit"
+        editbutton.innerHTML =
+        '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen-fill" viewBox="0 0 16 16"><path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001z"/></svg>'
 
         deletebutton.innerText ="Delete"
 
         deletebutton.addEventListener("click", () => deleteTask(task.id));
         editbutton.addEventListener("click", () => editTask(task.id));
-        checkbox.addEventListener("click", () => completed(task.id));
 
-        tableRow.append(createCell(task.id), createCell(task.title), checkbox);
+        tableRow.append(createCell(task.id), createCell(task.title), createCell(task.completed));
         
         tableBody.appendChild(tableRow);
-        tableRow.appendChild(deletebutton);
         tableRow.appendChild(editbutton);
-
+        tableRow.appendChild(deletebutton);
     });
 
 }
